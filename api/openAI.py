8c7dcc4b66f77b05149866456576e1701b01fdc7
AbeAlsop <abealsop@gmail.com>
2025-08-06 21:32:39 -0600

    Return two quotes

--- api/openAI.py
+++ api/openAI.py
@@ -13,15 +13,16 @@ def embed(text):
     result = open_ai_client.embeddings.create(input=text, model=EMBED_MODEL)
     return result.data
 
-#TODO: Detect when the input is too short to be a coherent thought, and respond with something like "Tell me more"
+#TODO: Find closest match from my descriptions of virtues, rather than asking OpenAI
 def get_virtue(text, context = []):
     messages = [
         {'role': 'system', 'content': '''
-        You are a counselor who helps people improve.
-        Select one of the following virtues or attributes that are most needed or desired by the user, and respond with nothing but the name of the selected virtue.
-        Body of Christ,Providence,Grace,Faith,Fidelity,Hope,Trust,Longing for God,Devotion,Scripture,Purity,Virtue,Acceptance,Contentment,Patience,Flexibility,Agility,Self-control,Detachment,Gratitude,Potential,Vision,Initiative,Planning,Preparation,Creativity,Resilience,Perseverance,Recovery,Innovation,Renewal,Inspiration,Ambition,Motivation,Opportunity,Direction,Fortitude,Growth,Learning,Education,Practice,Journey,Confidence,Eagerness,Courage,Dedication,Responsibility,Effort,Action,Accountability,Speed,Determination,Persistence,Diligence,Excellence,Accomplishment,Completion,Truth,Honesty,Wisdom,Focus,Awareness,Realism,Intelligence,Pondering,Vigilance,Discernment,Writing,Intuition,Imagination,Dignity,Mission,Priorities,Vocation,Adventure,Leisure,Rest,Nature,Health,Integrity,Humility,Self-awareness,Self-respect,Uniqueness,Originality,Caution,Mindfulness,Perspective,Attitude,Optimism,Happiness,Joy,Peace,Silence,Surprise,Wonder,Passion,Compassion,Relationship,Friendship,Love,Family,Parenting,Encouragement,Listening,Gentleness,Discretion,Respect,Commitment,Community,Communion of Saints,Justice,Kindness,Leadership,Forgiveness,Equality,Charity,Generosity,Simplicity,Teaching,Surrender,Sacrifice,Martyrdom
+        You are a counselor who helps people be more aware of the spiritual side of life and to be ready for their next calling. 
+        Select a virtue from the list below that is most relevant to the user, and respond with nothing but the name of the selected word.
+        For example, if someone is going on a vacation, then the relevant word might be "Journey". If someone is insulting himself without good cause, then the relevant word might be "Self-respect'.
+        Preparation,Body of Christ,Providence,Grace,Faith,Fidelity,Hope,Trust,Longing for God,Devotion,Scripture,Purity,Virtue,Prayer,Acceptance,Contentment,Patience,Flexibility,Agility,Self-control,Detachment,Gratitude,Potential,Vision,Initiative,Planning,Creativity,Resilience,Perseverance,Recovery,Renewal,Resurrection,Inspiration,Ambition,Motivation,Opportunity,Direction,Growth,Learning,Education,Practice,Confidence,Eagerness,Courage,Dedication,Responsibility,Effort,Action,Accountability,Speed,Determination,Persistence,Diligence,Excellence,Accomplishment,Completion,Truth,Honesty,Wisdom,Focus,Awareness,Realism,Intelligence,Pondering,Discernment,Writing,Intuition,Imagination,Dignity,Priorities,Vocation,Leisure,Rest,Nature,Health,Journey,Integrity,Humility,Self-awareness,Self-respect,Uniqueness,Caution,Mindfulness,Perspective,Attitude,Optimism,Joy,Peace,Silence,Surprise,Wonder,Passion,Compassion,Relationship,Friendship,Love,Family,Parenting,Listening,Gentleness,Discretion,Respect,Commitment,Community,Communion of Saints,Justice,Kindness,Leadership,Forgiveness,Equality,Charity,Generosity,Simplicity,Teaching,Surrender,Sacrifice,Martyrdom
         '''}
-    ]
+    ] #turn away from obsession with material things, and work to fulfill their spiritual calling.
     for history in context:
         messages.append({'role': 'user', 'content': history[0]})
         messages.append({'role': 'assistant', 'content': history[1]})
@@ -36,19 +37,20 @@ def get_virtue(text, context = []):
 
 # Context is a list of pairs
 #   The first element of each pair is the user request; the second is the response
-def apply_quote(text, assistant, context = []):
+def apply_quote(text, verses, context = []):
     messages = [
         {'role': 'system', 'content': '''
             You are a librarian who is connecting people to the works of famous authors and artists.
-            Your assistant has provided the quote that is most relevant to the user. Do not repeat the quote or find a new quote.
-            Simply give a brief explanation, no more than one sentence, of how that quote can be applied to the user's situation.
+            Your assistant has provided one or two relevant verses. Do not repeat them or find a new verse.
+            Simply give a brief explanation, no more than one sentence, of how the verses can be applied to the user's situation.
         '''}
     ]
     for history in context:
         messages.append({'role': 'user', 'content': history[0]})
         messages.append({'role': 'assistant', 'content': history[1]})
     messages.append({'role': 'user', 'content': text})
-    messages.append({'role': 'assistant', 'content': assistant})
+    for verse in verses:
+        messages.append({'role': 'assistant', 'content': verse})
 
     response = open_ai_client.chat.completions.create(
         model=LANG_MODEL,
--- api/quoteDB.py
+++ api/quoteDB.py
@@ -12,17 +12,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AWS_REGION = "us-east-1"
-INDEX_NAME = "wise-quotes-2"
+OLD_INDEX_NAME = "wise-quotes-2"
+COMBINED_INDEX_NAME = "wise-quotes-3"
+POP_INDEX_NAME = "wise-quotes-pop-3"
+FAITH_INDEX_NAME = "wise-quotes-faith-3"
+FAITH_GENRES = ["Saint","Bible","Christian","Song"]
+MIN_ID_TO_LOAD = 2129
 
 pinecone_client = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 pinecone_spec = ServerlessSpec(cloud="aws", region=AWS_REGION)
 
 class QuoteDB:
     def __init__(self):
-        self.index = pinecone_client.Index(INDEX_NAME)
+        self.old_index = self.get_or_create_index(OLD_INDEX_NAME)
+        self.pop_index = self.get_or_create_index(POP_INDEX_NAME)
+        self.faith_index = self.get_or_create_index(FAITH_INDEX_NAME)
+        self.combined_index = self.get_or_create_index(COMBINED_INDEX_NAME)
 
     def load_quotes(self):
-        with open('C:/dev/wiseverses/quotes.csv', 'r') as file:
+        with open('C:/dev/wiseverses/quotes.csv', 'r', encoding='utf-8') as file:
             quote_file = csv.reader(file)
             header = next(quote_file)
             quotes = []
@@ -30,28 +38,28 @@ class QuoteDB:
                 quote = {}
                 for i in range(0, len(header)):
                     quote[header[i]] = row[i]
-                quotes.append(quote)
+                if int(quote["ID"]) >= MIN_ID_TO_LOAD:
+                    quotes.append(quote)
             print(f"Loaded {len(quotes)} rows")
             return quotes
 
     def save_quotes_csv(self, data):
         numpy.savetxt("C:/dev/wiseverses/quotes.csv", [[d['author'],d.get('source',''),d['text']] for d in data], delimiter="|", fmt="%s")
 
-    def create_index(self, quotes):
+    def get_or_create_index(self, name):
         embed_dim = 1536
-
-        if INDEX_NAME not in pinecone_client.list_indexes().names():
+        if name not in pinecone_client.list_indexes().names():
             pinecone_client.create_index(
-                INDEX_NAME,
+                name,
                 dimension=embed_dim,
                 metric='dotproduct',
                 spec=pinecone_spec
             )
-
-        index = pinecone_client.Index(INDEX_NAME)
-        time.sleep(1)
+            time.sleep(1) # ensure index is created
         #print("Index stats:", index.describe_index_stats())
+        return pinecone_client.Index(name)
 
+    def add_quotes_to_indexes(self, quotes, indexes):
         batch_size = 32
         for i in tqdm(range(0, len(quotes), batch_size)):
             end = min(len(quotes), i + batch_size)
@@ -62,18 +70,35 @@ class QuoteDB:
             )
             embeds = [record.embedding for record in embed_result]
             vectors = list(zip(ids, embeds, quote_batch))
-            index.upsert(vectors)
-
-        print(f"Created index of {len(quotes)} quotes")
-        return index
+            for index in indexes:
+                index.upsert(vectors)
+        print(f"Added {len(quotes)} quotes to database")
 
     def rebuild_db(self):
         quote_data = self.load_quotes()
         # self.save_quotes_csv(quote_data)
-        self.create_index(quote_data)
+        self.add_quotes_to_indexes(
+            [quote for quote in quote_data if quote["Genre"] not in FAITH_GENRES],
+            [self.combined_index,self.pop_index]
+        )
+        self.add_quotes_to_indexes(
+            [quote for quote in quote_data if quote["Genre"] in FAITH_GENRES],
+            [self.combined_index,self.faith_index]
+        )
+
+    def find_first_match(self, prompt, index):
+        embedding = embed([prompt])[0].embedding
+        result = index.query(vector=[embedding], top_k=1, include_metadata=True)
+        return result['matches'][0]
 
     #TODO: Allow some variablility, and avoid duplicates within the same session
-    def lookup_quote(self, prompt, session):
-        embedding = embed([prompt])[0].embedding
-        result = self.index.query(vector=[embedding], top_k=1, include_metadata=True)
-        return result['matches'][0]['metadata']
+    def lookup_quote(self, prompt, session, style=""):
+        selected_index = self.pop_index if style=="pop" else self.faith_index if style=="faith" else self.combined_index
+        first_match = self.find_first_match(prompt, selected_index)
+        return first_match['metadata']
+
+    def lookup_pop_quote(self, prompt):
+        return self.find_first_match(prompt, self.pop_index)
+
+    def lookup_faith_quote(self, prompt):
+        return self.find_first_match(prompt, self.faith_index)
