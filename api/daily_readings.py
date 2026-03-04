0d2fcab1b0eff2edb96737b7b854fd469e89fd6d
AbeAlsop <abealsop@gmail.com>
2026-03-04 08:41:55 -0700

    v2 of relevant readings

--- api/daily_readings.py
+++ api/daily_readings.py
@@ -17,12 +17,15 @@ async def load_readings(day):
             if e.response.status_code == 404:
                 return f"No readings found for {day}"
 
+def transform_header(header):
+    return 'First reading' if header == 'Reading 1' else 'Second reading' if header == 'Reading 2' else header
+
 def get_readings(day):
-    response = asyncio.run(load_readings(day))
-    return response.to_dict()
+    response = asyncio.run(load_readings(day)).to_dict()
+    reading_dict = {transform_header(r['header']): r['readings'][0]['text'] for r in response['sections']}
+    return {'url': response['url'], 'readings': reading_dict}
 
 if __name__=="__main__":
+    #readings = get_readings(datetime.date(2026, 3, 19))
     readings = get_readings(datetime.date.today())
     print(readings)
-    reading_map = {r['header']: r['readings'][0]['text'] for r in readings['sections']}
-    print(reading_map)
